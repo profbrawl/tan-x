d1d5fd5088b8de672d64d28c33d83025299c495d
Jonathan Almeida <git@jonalmeida.com>
2015-12-13 07:52:28 -0500

    8 option menu + new anim + init multi-controller
     Conflicted:
    	Assets/Scenes/Home Screen.unity

--- Assets/Scripts/PickerManager.cs
+++ Assets/Scripts/PickerManager.cs
@@ -8,11 +8,19 @@ public class PickerManager : MonoBehaviour {
 	public GameObject cylinder;
 	public GameObject capsule;
 	public GameObject cube;
+	public GameObject sphere1;
+	public GameObject cylinder1;
+	public GameObject capsule1;
+	public GameObject cube1;
+	public GameObject playerOnePos;
+	public GameObject playerTwoPos;
 
-	private GameObject[] mCharacters = new GameObject[4];
-	private int mCurrentSelected = -1;
-	private bool mIsAxisInUse = false;
-	private bool mIsSelectedInUse = false;
+	private GameObject[,] mCharacters = new GameObject[4, 2];
+	private int mCurrentSelectedHorizontal = -1;
+	private int mCurrentSelectedVertical = 0;
+	private bool mIsHAxisInUse = false;
+	private bool mIsVAxisInUse = false;
+	private string mController = "j";
 
 	private static PickerManager instance = null;
 
@@ -23,103 +31,169 @@ public class PickerManager : MonoBehaviour {
 		} else if (instance != this) {
 			Destroy(gameObject);
 		}
-		mCharacters[0] = sphere;
-		mCharacters[1] = cylinder;
-		mCharacters[2] = capsule;
-		mCharacters[3] = cube;
+
+//		StartCoroutine(WaitForSelector());
+
+		mCharacters[0, 0] = sphere;
+		mCharacters[1, 0]= cylinder;
+		mCharacters[2, 0] = capsule;
+		mCharacters[3, 0] = cube;
+		mCharacters[0, 1] = cube1;
+		mCharacters[1, 1] = capsule1;
+		mCharacters[2, 1] = sphere1;
+		mCharacters[3, 1]= cylinder1;
+
+//		if (Input.GetButton("Jump"))
+//			mController = "k";
+
+//		else if (Input.GetButton("jSubmit"))
+//			mController = "j";
 	}
-	
+
+	IEnumerator WaitForSelector() {
+		while (true) {
+			if (Input.GetButton("Jump")) {
+				Debug.Log("Main controller is keyboard");
+				mController = "k";
+				break;
+			} else if (Input.GetButton("jSubmit")) {
+				Debug.Log("Main controller is joystick");
+				mController = "j";
+				break;
+			} else {
+				yield return null;
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
-		float clicked = Input.GetAxisRaw("Horizontal");
-		//Debug.Log("Input axis value: " + clicked);
-		if(clicked == 0) {
-			mIsAxisInUse = false;
-		} else if(clicked < 0) { // We moved our controller joystick left
-			if (!initalSelection) {
-				if (mIsAxisInUse == false) {
-					mIsAxisInUse = true;
-					animateBackward(mCurrentSelected);
-					int nextMovePosition = (((mCurrentSelected - 1) % 4) + 4) % 4;
-					animateForward(nextMovePosition);
-					mCurrentSelected = nextMovePosition;
+		float verticalAxis = Input.GetAxisRaw(mController + "Vertical");
+		if (verticalAxis == 0) {
+			mIsVAxisInUse = false;
+		} else if (verticalAxis < 0) { // We move our controller joystick down
+			if (initalSelection) {
+				if (mIsVAxisInUse == false) { 
+					mIsVAxisInUse = true;
+					mCurrentSelectedVertical = 0;
+					animateForward(0, mCurrentSelectedVertical);
+					initalSelection = false;
 				}
 			} else {
-				if (mIsAxisInUse == false) {
-					mIsAxisInUse = true;
-					mCurrentSelected = 3;
-					mCharacters[3].GetComponent<Animation>().Blend("Cube_SelectedForward");
+				if (mIsVAxisInUse == false && mCurrentSelectedVertical != 0) {
+					mIsVAxisInUse = true;
+					animateBackward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+					mCurrentSelectedVertical = 0;
+					animateForward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+				}
+			}
+		} else { // We move our controller joystick up
+			if (initalSelection) {
+				if (mIsVAxisInUse == false) { 
+					mIsVAxisInUse = true;
+					mCurrentSelectedVertical = 1;
+					animateForward(0, mCurrentSelectedVertical);
+					initalSelection = false;
+				}
+			} else {
+				if (mIsVAxisInUse == false && mCurrentSelectedVertical != 1) {
+					mIsVAxisInUse = true;
+					animateBackward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+					mCurrentSelectedVertical = 1;
+					animateForward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+				}
+			}
+		}
+
+		float horizontalAxis = Input.GetAxisRaw(mController + "Horizontal");
+		if (horizontalAxis == 0) {
+			mIsHAxisInUse = false;
+		} else if(horizontalAxis < 0) { // We moved our controller joystick left
+			if (!initalSelection) {
+				if (mIsHAxisInUse == false) {
+					mIsHAxisInUse = true;
+					animateBackward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+					int nextMovePosition = (((mCurrentSelectedHorizontal - 1) % 4) + 4) % 4;
+					animateForward(nextMovePosition, mCurrentSelectedVertical);
+					mCurrentSelectedHorizontal = nextMovePosition;
+				}
+			} else {
+				if (mIsHAxisInUse == false) {
+					mIsHAxisInUse = true;
+					mCurrentSelectedHorizontal = 3;
+					animateForward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
 					initalSelection = false;
 				}
 			}
 		} else { // We moved our controller joystick right
 			if (!initalSelection) {
-				if (mIsAxisInUse == false) {
-					mIsAxisInUse = true;
-					animateBackward(mCurrentSelected);
-					int nextMovePosition = (mCurrentSelected + 1) % 4;
-					animateForward(nextMovePosition);
-					mCurrentSelected = nextMovePosition;
+				if (mIsHAxisInUse == false) {
+					mIsHAxisInUse = true;
+					animateBackward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+					int nextMovePosition = (mCurrentSelectedHorizontal + 1) % 4;
+					animateForward(nextMovePosition, mCurrentSelectedVertical);
+					mCurrentSelectedHorizontal = nextMovePosition;
 				}
 			} else { // Bring left most character forward
-				if (mIsAxisInUse == false) {
-					mIsAxisInUse = true;
-					mCurrentSelected = 0;
-					mCharacters[0].GetComponent<Animation>().Blend("Sphere_SelectedForward");
+				if (mIsHAxisInUse == false) {
+					mIsHAxisInUse = true;
+					mCurrentSelectedHorizontal = 0;
+					animateForward(mCurrentSelectedHorizontal, mCurrentSelectedVertical);
+					//mCharacters[0].GetComponent<Animation>().Blend("Sphere_SelectedForward");
 					initalSelection = false;
 				}
 			}
 		}
 
-		if (Input.GetKey(KeyCode.KeypadEnter) || Input.GetButtonDown("Submit")) {
-			Debug.Log("Submit button");
+		if (Input.GetButtonDown(mController + "Submit")) {
+			Debug.Log("Submit button hit");
+			//TODO Pass the selection to the GameScene
 			Application.LoadLevel("GameScene");
 		}
 			
 	}
 
-	private void animateForward(int position) {
-		switch(position) {
+	private void animateForward(int xPos, int yPos) {
+		StartCoroutine(MoveObject(
+			mCharacters[xPos, yPos].transform, 
+			mCharacters[xPos, yPos].transform.position, 
+			playerOnePos.transform.position, 0.2f));
+	}
+
+	private void animateBackward(int xPos, int yPos) {
+		float vecYPosition = mCurrentSelectedVertical == 0 ? 0f : 3f;
+		switch(xPos) {
 			case 0:
-				mCharacters[0].GetComponent<Animation>().Blend("Sphere_SelectedForward");
+				StartCoroutine(MoveObject(getTransform(xPos, yPos), getTransform(xPos, yPos).position,
+					new Vector3(-4.75f, vecYPosition, 5), 0.2f));
 				break;
 			case 1:
-				mCharacters[1].GetComponent<Animation>().Blend("Cylinder_SelectedForward");
+				StartCoroutine(MoveObject(getTransform(xPos, yPos), getTransform(xPos, yPos).position,
+					new Vector3(-1.80f, vecYPosition, 5), 0.2f));
 				break;
 			case 2:
-				mCharacters[2].GetComponent<Animation>().Blend("Capsule_SelectedForward");
+				StartCoroutine(MoveObject(getTransform(xPos, yPos), getTransform(xPos, yPos).position,
+					new Vector3(1.42f, vecYPosition, 5), 0.2f));
 				break;
 			case 3:
-				mCharacters[3].GetComponent<Animation>().Blend("Cube_SelectedForward");
-				break;
-			default:
+				StartCoroutine(MoveObject(getTransform(xPos, yPos), getTransform(xPos, yPos).position,
+					new Vector3(4.53f, vecYPosition, 5), 0.2f));
 				break;
 		}
 	}
 
-	private void animateBackward(int position) {
-		switch(position) {
-			case 0:
-				mCharacters[0].GetComponent<Animation>().Blend("Sphere_SelectedBackward");
-				break;
-			case 1:
-				mCharacters[1].GetComponent<Animation>().Blend("Cylinder_SelectedBackward");
-				break;
-			case 2:
-				mCharacters[2].GetComponent<Animation>().Blend("Capsule_SelectedBackward");
-				break;
-			case 3:
-				mCharacters[3].GetComponent<Animation>().Blend("Cube_SelectedBackward");
-				break;
-			default:
-				break;
+	private IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time) {
+		float i = 0.0f;
+		float rate = 1.0f/time;
+		while (i < 1.0) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null;
 		}
 	}
 
-	private void animateSelected(int position) {
-		if (position != -1) { // No item has been selected yet.
-			mCharacters[position].GetComponent<Animation>().Blend("SelectedEnter");
-		}
+	private Transform getTransform(int x, int y) {
+		return mCharacters[x, y].transform;
 	}
 }
