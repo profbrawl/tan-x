474029089d7ac03c732f1a1b4f860b44ba7e8006
Will Roeder <roedz93@gmail.com>
2015-12-13 23:29:12 -0500

    When players die they now respawn properly. Scores are updated and the game should end at first = 10

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,105 +5,62 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
 	// Public variables
-	public int numberOfTanks = 1;
-	public GameObject[] players = new GameObject[2];
     private int playerOneScore = 0;
     private int playerTwoScore = 0;
-    public Text playerOneText;
-    public Text playerTwoText;
-    public GameObject gameOverText;
+	public Text playerOneText;
+	public Text playerTwoText;
+	public GameObject gameOverText;
     public bool gamePaused = false;
-    private const int WIN_SCORE = 300;
+	private const int WIN_SCORE = 10; 
 
-	public static GameManager instance = null;
-
-	// Use this for initialization
-	void Awake () {
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-			Destroy (gameObject);
-
-		Setup (0); // temp until we get more maps. Will randomize.
-	}    
-	
-	public void Setup(int mapID) {                     
-		SetupTanks();
-	}
-	
-	void Update () {        
-        if(!gamePaused)
-        {
-            //AddToScore(1);
-            //AddToScore(2);
-        }        
-	}
-
-	void SetupTanks() {
-		Instantiate (players [0]).transform.forward = new Vector3 (0,0,1);
-		Instantiate (players [1]).transform.forward = new Vector3 (0,0,1);
-	}
-	
-    void AddToScore(int player)
-    {
-        if (player == 1)
-        {
+    public void AddToScore(int player) {
+        if (player == 1) {
             playerOneText.text = "Player 1 Kills: " + ++playerOneScore;
-        }
-        else
-        {
+        } else {
             playerTwoText.text = "Player 2 Kills: " + ++playerTwoScore;
         }
         CheckGameOver();
     }
 
-    void CheckGameOver()
-    {
+	public void CheckGameOver() {
         Text winnerText = gameOverText.GetComponent("Text") as Text;
-        if (playerOneScore == WIN_SCORE)
-        {
+
+        if (playerOneScore == WIN_SCORE) {
             winnerText.text = "Player 1 Wins!";
             gameOverText.SetActive(true);
             PauseGame();  
-        }else if(playerTwoScore == WIN_SCORE)
-        {
+        } else if(playerTwoScore == WIN_SCORE) {
             winnerText.text = "Player 2 Wins!";
             gameOverText.SetActive(true);
             PauseGame();
         }
     }
 
-    void PauseGame()
-    {
+	public void PauseGame() {
         Time.timeScale = 0;
         gamePaused = true;
     }
 
-    void UnPauseGame()
-    {
+	public void UnPauseGame() {
         Time.timeScale = 1;
         gamePaused = false;
     }
 
-    void RestartGame()
-    {
+	public void RestartGame() {
         Application.LoadLevel(Application.loadedLevel);
         UnPauseGame();
     }
 
-    void LoadMainMenu()
-    {
+	public void LoadMainMenu() {
         Application.LoadLevel("Home Screen");
         UnPauseGame();
     }
 
-    public void Restart_Click()
-    {
+    public void Restart_Click() {
         RestartGame();
     }
 
-    public void MainMenu_Click()
-    {
+    public void MainMenu_Click() {
         LoadMainMenu();
     }
 
---
+++ Assets/Scripts/GameScene1_Manager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameScene1_Manager : GameManager {
+	
+	public GameObject[] players = new GameObject[2];
+
+	private GameObject player1;
+	private GameObject player2;
+
+	public static GameManager instance = null;
+	
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		SetupPlayer1 ();
+		SetupPlayer2 ();
+	}    
+	
+	void Update () {        
+        if(!gamePaused) {
+			player1.GetComponent<Vehicle>().getHealth();
+			if (player1.GetComponent<Vehicle>().getHealth() <= 0) {
+				AddToScore(2);
+				Destroy(player1);
+				SetupPlayer1();
+			} else if (player2.GetComponent<Vehicle>().getHealth() <= 0) {
+				AddToScore(1);
+				Destroy(player2);
+				SetupPlayer2();
+			}
+        }
+		CheckGameOver ();
+	}
+
+	void SetupPlayer1() {
+		player1 = Instantiate (players [0]);
+	}
+
+	void SetupPlayer2() {
+		player2 = Instantiate (players [1]);
+	}
+}
